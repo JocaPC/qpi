e6e869da39b4d46ec153f211bd70160600cc3831
Jovan Popovic <jocapc@gmail.com>
2019-01-10 22:40:45 +0100

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,13 +1,13 @@
 $m = "Version - $(Get-Date -Format g)"
 git checkout master
 .\mcpp.exe -k -P -o ..\src\qpi.sql -D AZURE -D MI qpi.tmpl.sql
-git commit -a -m "$m"
+git commit -a -m "Deploying new version"
 git checkout azure-db
 .\mcpp.exe -k -P -o ..\src\qpi.sql -D AZURE -D DB qpi.tmpl.sql
-git commit -a -m "$m"
+git commit -a -m "Deploying new version"
 git checkout sql2017
 .\mcpp.exe -k -P -o ..\src\qpi.sql -D SQL2017 qpi.tmpl.sql
-git commit -a -m "$m"
+git commit -a -m "Deploying new version"
 git checkout sql2016
 .\mcpp.exe -k -P -o ..\src\qpi.sql -D SQL2016 qpi.tmpl.sql
-git commit -a -m "$m"
+git commit -a -m "Deploying new version"

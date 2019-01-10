2523b1a45f1e4e5df468d77a3b74823d73af3a02
Jovan Popovic <jocapc@gmail.com>
2019-01-10 20:37:51 +0100

    Create build.bat

---
+++ build/build.bat
@@ -0,0 +1,16 @@
+git checkout master
+.\azmi.bat
+git commit
+
+git checkout azure-db
+.\azdb.bat
+git commit
+
+git checkout sql2017
+.\sql2017.bat
+git commit
+
+git checkout sql2016
+.\sql2016.bat
+git commit
+

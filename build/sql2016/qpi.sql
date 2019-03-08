2f158ba251f98b7e45b88f5ba407dd5f8a8ab66e
Jovan Popovic <jocapc@gmail.com>
2019-03-08 16:32:32 +0100

    Updating qpi.volumes

--- build/sql2016/qpi.sql
+++ build/sql2016/qpi.sql
@@ -1036,9 +1036,9 @@ GO
 CREATE  VIEW qpi.volumes
 AS
 SELECT	volume_mount_point,
-		used_gb = MIN(total_bytes / 1024 / 1024 / 1024),
-		available_gb = MIN(available_bytes / 1024 / 1024 / 1024),
-		total_gb = MIN((total_bytes+available_bytes) / 1024 / 1024 / 1024)
+		used_gb = CAST(MIN(total_bytes / 1024. / 1024 / 1024) AS NUMERIC(8,1)),
+		available_gb = CAST(MIN(available_bytes / 1024. / 1024 / 1024) AS NUMERIC(8,1)),
+		total_gb = CAST(MIN((total_bytes+available_bytes) / 1024. / 1024 / 1024) AS NUMERIC(8,1))
 FROM sys.master_files AS f
 CROSS APPLY sys.dm_os_volume_stats(f.database_id, f.file_id)
 GROUP BY volume_mount_point;

a65178495b7d5a1a7e465b1869993d62f7c21d4b
Jovan Popovic <jocapc@gmail.com>
2019-04-28 12:57:27 +0200

    Added query_hash to db_query_plans

--- build/sql2016/qpi.sql
+++ build/sql2016/qpi.sql
@@ -126,7 +126,7 @@ CREATE  VIEW qpi.db_query_plans
 as
 select	q.text, q.params, q.query_text_id, p.plan_id, p.query_id,
 		p.compatibility_level, p.query_plan_hash, p.count_compiles,
-		p.is_parallel_plan, p.is_forced_plan, p.query_plan
+		p.is_parallel_plan, p.is_forced_plan, p.query_plan, q.query_hash
 from sys.query_store_plan p
 	join qpi.db_queries q
 		on p.query_id = q.query_id;
@@ -134,7 +134,7 @@ GO
 
 CREATE  VIEW qpi.db_query_plans_ex
 as
-select	q.text, q.params, q.query_text_id, p.*
+select	q.text, q.params, q.query_text_id, p.*, q.query_hash
 from sys.query_store_plan p
 	join qpi.db_queries q
 		on p.query_id = q.query_id;

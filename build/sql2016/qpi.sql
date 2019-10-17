d7bd2b89c2a5f3e4a65a0e3e6917896ec8a4e42a
Jovan Popovic <jocapc@gmail.com>
2019-10-17 10:48:16 +0200

    Improving hints

--- build/sql2016/qpi.sql
+++ build/sql2016/qpi.sql
@@ -171,24 +171,36 @@ WHERE text NOT LIKE '%qpi.queries%'
 GO
 
 CREATE
-PROCEDURE qpi.force_db_plan @query_id int, @plan_id int = null, @hints nvarchar(4000) = null
+PROCEDURE qpi.clear_db_queries
+AS BEGIN
+	ALTER DATABASE current SET QUERY_STORE CLEAR;
+END
+GO
+
+CREATE
+PROCEDURE [qpi].[force] @query_id int, @plan_id int = null, @hints nvarchar(4000) = null
 AS BEGIN
 	declare @guide sysname = CONCAT('FORCE', @query_id),
 			@sql nvarchar(max),
 			@param nvarchar(max),
-			@exists bit;
+			@exists bit = 0;
 	select @sql = text, @param =  IIF(LEFT(params,1) = '(', SUBSTRING( params, 2, (PATINDEX( '%)[^),]%', params+')'))-2), "")
 	from qpi.db_queries
 	where query_id = @query_id;
-	select @guide = name, @exists = 1 from sys.plan_guides where query_text = @sql;
-	if (@exists = 1)
+	select @guide = name, @exists = 1 from sys.plan_guides where query_text = @sql or name = @guide;
+	if (@exists = 1) begin
+		PRINT 'Removing existing plan guide ' + @guide;
 		EXEC sp_control_plan_guide N'DROP', @guide;
+	end;
 
 	if(@plan_id is not null)
 		EXEC sp_query_store_force_plan @query_id, @plan_id;
 	else if (@hints is not null)
 	begin
 		SET @param = IIF(@param = "", null, @param);
+		IF substring(@hints, 1,7) <> 'OPTION'
+			SET @hints = 'OPTION(' + @hints + ')';
+		PRINT 'Forcing hint ' + @hints + ' on query ' + @sql;
 		EXEC sp_create_plan_guide @name = @guide,
 			@stmt = @sql,
 			@type = N'SQL',

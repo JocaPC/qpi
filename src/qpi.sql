3022cd028889952aa7388ebfcf121199a0dfd3c4
Jovan Popovic <jocapc@gmail.com>
2019-11-12 08:47:50 -0800

    Added qpi.query_stats

--- src/qpi.sql
+++ src/qpi.sql
@@ -164,10 +164,46 @@ SELECT
 		database_id, connection_id, session_id, request_id, command,
 		interval_mi = null,
 		start_time,
-		end_time = null
+		end_time = null,
+		sql_handle
 FROM    sys.dm_exec_requests
 		CROSS APPLY sys.dm_exec_sql_text(sql_handle)
 WHERE text NOT LIKE '%qpi.queries%'
+GO
+
+CREATE OR ALTER  VIEW qpi.query_stats
+AS
+select q.text, q.params, q.query_id, q.session_id, q.request_id, q.memory_mb, q.start_time,
+		duration_s = CAST(ROUND( rs.total_elapsed_time/execution_count /1000.0 /1000.0, 2) AS NUMERIC(12,2)),
+        cpu_time_ms = CAST(ROUND(rs.total_worker_time/execution_count, 1) AS NUMERIC(12,1)),
+        logical_io_reads_kb = CAST(ROUND(rs.total_logical_reads/execution_count * 8 /1000.0, 2) AS NUMERIC(12,2)),
+        logical_io_writes_kb = CAST(ROUND(rs.total_logical_writes/execution_count * 8 /1000.0, 2) AS NUMERIC(12,2)),
+        physical_io_reads_kb = CAST(ROUND(rs.total_physical_reads/execution_count * 8 /1000.0, 2) AS NUMERIC(12,2)),
+        clr_time_ms = CAST(ROUND(rs.total_clr_time/execution_count /1000.0, 1) AS NUMERIC(12,1)),
+		granted_mb = rs.total_grant_kb/execution_count /1024,
+		used_mb = rs.total_used_grant_kb/execution_count /1024,
+		ideal_mb = rs.total_ideal_grant_kb/execution_count /1024
+from qpi.queries q
+left join sys.dm_exec_query_stats rs
+on q.sql_handle = rs.sql_handle
+GO
+
+CREATE OR ALTER  VIEW qpi.query_mem_grants
+AS
+select q.text, q.params, q.query_id, q.session_id, q.request_id, q.memory_mb, q.start_time,
+		required_mb = mg.required_memory_kb /1024,
+		requested_mb = mg.requested_memory_kb /1024,
+		granted_mb = mg.granted_memory_kb /1024,
+		used_mb = mg.used_memory_kb /1024,
+		max_used_mb = mg.max_used_memory_kb /1024,
+		ideal_mb = mg.ideal_memory_kb /1024,
+		timeout_s = mg.timeout_sec,
+		mg.wait_time_ms,
+		mg.is_next_candidate
+from qpi.queries q
+left join sys.dm_exec_query_memory_grants mg
+on q.session_id = mg.session_id
+and q.request_id = mg.request_id
 GO
 
 CREATE OR ALTER
